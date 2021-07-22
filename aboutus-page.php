a2791f72edfe6927a2e5cf15442b550f23c2bae2
Andrey <tynyanyi@mail.ru>
2021-07-22 20:07:27 +0300

    допилина страница о нас

--- aboutus-page.php
+++ aboutus-page.php
@@ -10,12 +10,23 @@ get_header();
 /**
  * Получаем все кастомные поля для текущей записи
  */
-//$custom_fields = get_post_custom();
-//print_r($custom_fields);
+$custom_fields = get_post_custom();
 //var_dump($custom_fields['kartinka_k_napravleniyu'][0]);
 //$class_modifikator = get_field('vybor_napravleniya');
 
 $the_post = get_post();
+/**
+ *  [title_1] => Array ( [0] => Развить )
+ *  [text_1] => Array ( [0] => интеллектуальные способности, логику и аналитическое мышление детей )
+ *  [title_2] => Array ( [0] => Научить )
+ *  [text_2] => Array ( [0] => ребят учиться самостоятельно с удовольствием и интересом! )
+ *  [title_3] => Array ( [0] => Способствовать )
+ *  [text_3] => Array ( [0] => формированию у учащихся адекватной самооценки, умению отстаивать свою точку зрения, уважая при этом мнение других. )
+ *  [title_4] => Array ( [0] => Воспитать )
+ *  [text_4] => Array ( [0] => характер победителя: сильную волю, умение справляться с неудачами, не зазнаваться при победах, все время учиться и двигаться вперед к новым вершинам. )
+ *  [title_5] => Array ( [0] => Подготовить )
+ *  [text_5] => Array ( [0] => ко взрослой жизни человека, способного решать сложные задачи и добиваться поставленных целей честным путем )
+ */
 ?>
 
     <!-- START CONTENT-->
@@ -28,7 +39,50 @@ $the_post = get_post();
                 <?= $the_post->post_content;?>
             </div>
             <div class="about-page-img">
-
+                <div class="grid-item post-1">
+                    <div class="grid-item-text">
+                        <h4><?=$custom_fields['title_1'][0]?></h4>
+                        <p><?=$custom_fields['text_1'][0]?></p>
+                    </div>
+                </div>
+                <div class="grid-item post-2">
+                </div>
+                <div class="grid-item post-3">
+                    <div class="grid-item-text">
+                        <h4><?=$custom_fields['title_2'][0]?></h4>
+                        <p><?=$custom_fields['text_2'][0]?></p>
+                    </div>
+                </div>
+                <div class="grid-item post-4">
+                    <div class="grid-item-text">
+                        <h4><?=$custom_fields['title_3'][0]?></h4>
+                        <p><?=$custom_fields['text_3'][0]?></p>
+                    </div>
+                </div>
+                <div class="grid-item post-5">
+                    <img src="<?php bloginfo('template_url');?>/assets/img/mishen.png" alt="" srcset="">
+                    <img class="arow-green" src="<?php bloginfo('template_url');?>/assets/img/arow-green.png" alt="" srcset="">
+                    <img class="arow-red" src="<?php bloginfo('template_url');?>/assets/img/arow-red.png" alt="" srcset="">
+                    <img class="arow-blue" src="<?php bloginfo('template_url');?>/assets/img/arow-blue.png" alt="" srcset="">
+                    <img class="arow-grey" src="<?php bloginfo('template_url');?>/assets/img/arow-grey.png" alt="" srcset="">
+                    <img class="arow-orange" src="<?php bloginfo('template_url');?>/assets/img/arow-orange.png" alt="" srcset="">
+                </div>
+                <div class="grid-item post-6">
+                </div>
+                <div class="grid-item post-7">
+                </div>
+                <div class="grid-item post-8">
+                    <div class="grid-item-text">
+                        <h4><?=$custom_fields['title_4'][0]?></h4>
+                        <p><?=$custom_fields['text_4'][0]?></p>
+                    </div>
+                </div>
+                <div class="grid-item post-9">
+                    <div class="grid-item-text">
+                        <h4><?=$custom_fields['title_5'][0]?></h4>
+                        <p><?=$custom_fields['text_5'][0]?></p>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
